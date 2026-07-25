302094588504d61406c0018c79e7d0f46660bed1
Arankd <condori.9919187@gmail.com>
2026-07-24 23:05:22 -0400

    html ayuda con IA

---
+++ js/secc.js
@@ -0,0 +1,20 @@
+const linknav = document.querySelectorAll('nav ul li a');
+const secciones = document.querySelectorAll('.seccion');
+
+linknav.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const href = link.getAttribute('href');
+        const idSeccion = href.substring(1);
+
+        secciones.forEach(sec => {
+            sec.classList.remove('activa');
+            sec.classList.add('oculta');
+        });
+
+        const seccionActiva = document.getElementById(idSeccion);
+        seccionActiva.classList.add('activa');
+        seccionActiva.classList.remove('oculta');
+    });
+});
