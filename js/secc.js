6a91a9507da4d9c3b99a4c645520d803273e68ec
Arankd <condori.9919187@gmail.com>
2026-07-27 13:23:44 -0400

    error href

--- js/secc.js
+++ js/secc.js
@@ -1,45 +1,29 @@
-const linknav = document.querySelectorAll('nav ul li a');
+
+const linksDeNavegacion = document.querySelectorAll('nav ul li a, .leng');
 const secciones = document.querySelectorAll('.seccion');
 
-linknav.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
+function cambiarSeccion(evento) {
+    evento.preventDefault();
 
-        const href = link.getAttribute('href');
-        const idSeccion = href.substring(1);
+    const href = this.getAttribute('href');
+    const idSeccion = href.substring(1);
 
-        secciones.forEach(sec => {
-            sec.classList.remove('activa');
-            sec.classList.add('oculta');
-        });
-
-        const seccionActiva = document.getElementById(idSeccion);
-        seccionActiva.classList.add('activa');
-        seccionActiva.classList.remove('oculta');
-
-        // Añade esta línea para actualizar la URL sin recargar la página
-        history.pushState(null, '', href);
+    secciones.forEach(sec => {
+        sec.classList.remove('activa');
+        sec.classList.add('oculta');
     });
-});
-const inisec = document.querySelectorAll('.leng');
 
-inisec.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        
-        const href = link.getAttribute('href');
-        const idSeccion = href.substring(1);
-
-        secciones.forEach(sec => {
-            sec.classList.remove('activa');
-            sec.classList.add('oculta');
-        });
-
-        const seccionActiva = document.getElementById(idSeccion);
+    const seccionActiva = document.getElementById(idSeccion);
+    if (seccionActiva) {
         seccionActiva.classList.add('activa');
         seccionActiva.classList.remove('oculta');
+    }
 
-        history.pushState(null, '', href);
+    history.pushState(null, '', href);
+}
+
+linksDeNavegacion.forEach(link => {
+    link.addEventListener('click', (e) => {
+        cambiarSeccion.call(this, e);
     })
-})
-
+});
