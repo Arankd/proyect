f908f94fd5eb361df0e3edb998f7c61889f490f7
Arankd <condori.9919187@gmail.com>
2026-07-27 12:58:25 -0400

    ver 2

--- js/aside.js
+++
@@ -1,9 +0,0 @@
-const botonDesplegar = document.querySelector('#btn-des');
-const panelLateral = document.querySelector('#asides');
-
-if (botonDesplegar && panelLateral) {
-
-  botonDesplegar.addEventListener('click', () => {
-    panelLateral.classList.toggle('visible');
-  });
-}
