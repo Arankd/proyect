31d2e90e991517d08eb6e58dc89a6d64fa05c947
Arankd <condori.9919187@gmail.com>
2026-07-27 13:34:27 -0400

    error

--- js/secc.js
+++ js/secc.js
@@ -1,29 +1,43 @@
-
-const linksDeNavegacion = document.querySelectorAll('nav ul li a, .leng');
+const linknav = document.querySelectorAll('nav ul li a');
 const secciones = document.querySelectorAll('.seccion');
 
-function cambiarSeccion(evento) {
-    evento.preventDefault();
-
-    const href = this.getAttribute('href');
-    const idSeccion = href.substring(1);
-
-    secciones.forEach(sec => {
-        sec.classList.remove('activa');
-        sec.classList.add('oculta');
-    });
-
-    const seccionActiva = document.getElementById(idSeccion);
-    if (seccionActiva) {
-        seccionActiva.classList.add('activa');
-        seccionActiva.classList.remove('oculta');
-    }
-
-    history.pushState(null, '', href);
-}
-
-linksDeNavegacion.forEach(link => {
+linknav.forEach(link => {
     link.addEventListener('click', (e) => {
-        cambiarSeccion.call(this, e);
-    })
+        e.preventDefault();
+
+        const href = link.getAttribute('href');
+        const idSeccion = href.substring(1);
+
+        secciones.forEach(sec => {
+            sec.classList.remove('activa');
+            sec.classList.add('oculta');
+        });
+
+        const seccionActiva = document.getElementById(idSeccion);
+        if (seccionActiva) {
+            seccionActiva.classList.add('activa');
+            seccionActiva.classList.remove('oculta');
+        }
+    });
 });
+const inisec = document.querySelectorAll('.leng');
+
+inisec.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        const href = link.getAttribute('href');
+        const idSeccion = href.substring(1);
+
+        secciones.forEach(sec => {
+            sec.classList.remove('activa');
+            sec.classList.add('oculta');
+        });
+
+        const seccionActiva = document.getElementById(idSeccion);
+        if (seccionActiva) { 
+            seccionActiva.classList.add('activa');
+            seccionActiva.classList.remove('oculta');
+        }
+    })
+})
