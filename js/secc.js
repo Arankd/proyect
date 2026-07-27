91a47a17a0b84177635f657e4244a137aa7cf7a3
Arankd <condori.9919187@gmail.com>
2026-07-27 13:00:54 -0400

    ver 3

---
+++ js/secc.js
@@ -0,0 +1,40 @@
+const linknav = document.querySelectorAll('nav ul li a');
+const secciones = document.querySelectorAll('.seccion');
+
+linknav.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const href = link.getAttribute('href');
+        const idSeccion = href.substring(1);
+
+        secciones.forEach(sec => {
+            sec.classList.remove('activa');
+            sec.classList.add('oculta');
+        });
+
+        const seccionActiva = document.getElementById(idSeccion);
+        seccionActiva.classList.add('activa');
+        seccionActiva.classList.remove('oculta');
+    });
+});
+const inisec = document.querySelectorAll('.leng');
+
+inisec.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        const href = link.getAttribute('href');
+        const idSeccion = href.substring(1);
+
+        secciones.forEach(sec => {
+            sec.classList.remove('activa');
+            sec.classList.add('oculta');
+        });
+
+        const seccionActiva = document.getElementById(idSeccion);
+        seccionActiva.classList.add('activa');
+        seccionActiva.classList.remove('oculta');
+    })
+})
+
